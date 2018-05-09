de6732443321004a2a9cf7dffa56587dedef79ce
Kim Lyndon <kimmylyndon@gmail.com>
2018-05-09 10:03:55 -0400

    First commit

--- MemeMe1/ViewController.swift
+++ MemeMe1/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+//Set outlet for imageView
+    @IBOutlet weak var imagePickerView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let cameraButton = UIBarButtonItem()
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+//imagePickerControllerDelegate Methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+//Dismiss picker after choice is made
+    self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_: UIImagePickerController) {
+        
+//Dismiss picker when user chooses CANCEL
+     self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+//Connect action for the ALBUM button and set delegate
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+//Distinguish betweeh album and camera
+        imagePicker.sourceType = .photoLibrary
+        
+//Displays screen for user photo choice
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+//Connect action for camera option
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
-}
 
